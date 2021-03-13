1e9ab4d098f7df4501e18d74793a4a6e0e7ed4ec
kunalmnnit <ksevkani495@gmail.com>
2021-03-13 11:28:04 +0530

    Login SignUp Added without testing

---
+++ app/src/main/java/com/kunal/icare/LoginActivity.kt
@@ -0,0 +1,52 @@
+ package com.kunal.icare
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.kunal.icare.databinding.ActivityLoginBinding
+
+ class LoginActivity : AppCompatActivity() {
+     private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        val auth = Firebase.auth
+        if(auth.currentUser!=null) {
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        }
+        else
+            setContentView(view)
+        binding.signUp.setOnClickListener {
+            val i = Intent(this@LoginActivity, SignupActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+        binding.signIn.setOnClickListener {
+            login()
+        }
+    }
+
+     private fun login() {
+         val auth = Firebase.auth
+         auth.signInWithEmailAndPassword(binding.emailAddress.getText().toString(), binding.password.getText().toString()).addOnCompleteListener { task ->
+             if (task.isSuccessful) {
+                 Toast.makeText(this@LoginActivity, "User Successfully logged in", Toast.LENGTH_SHORT).show()
+                 val user = auth.currentUser!!.uid
+//                 val db = Firebase.firestore
+//                 db.collection("patients").document(user).update("flag",false)
+                 startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                 finish()
+             } else {
+                 Toast.makeText(this@LoginActivity, "Wrong Email Address or Password", Toast.LENGTH_SHORT).show()
+             }
+         }
+     }
+
+     override fun onBackPressed() {
+         finish()
+     }
+}
